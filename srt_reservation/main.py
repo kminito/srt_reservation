d774b00827b66925d6b74dac858313c98e41738b
kminito <infinitic@naver.com>
2022-09-13 21:01:15 +0900

    refactoring methods

--- srt_reservation/main.py
+++ srt_reservation/main.py
@@ -66,10 +66,8 @@ class SRT:
         except WebDriverException:
             self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
-
     def login(self):
         self.driver.get('https://etk.srail.co.kr/cmc/01/selectLoginForm.do')
-
         self.driver.implicitly_wait(15)
         self.driver.find_element(By.ID, 'srchDvNm01').send_keys(str(self.login_id))
         self.driver.find_element(By.ID, 'hmpgPwdCphd01').send_keys(str(self.login_psw))
@@ -84,10 +82,7 @@ class SRT:
         else:
             return False
 
-
-
     def go_search(self):
-
         # 기차 조회 페이지로 이동
         self.driver.get('https://etk.srail.kr/hpg/hra/01/selectScheduleList.do')
         self.driver.implicitly_wait(5)
@@ -116,12 +111,55 @@ class SRT:
         print(f"출발역:{self.dpt_stn} , 도착역:{self.arr_stn}\n날짜:{self.dpt_dt}, 시간: {self.dpt_tm}시 이후\n{self.num_trains_to_check}개의 기차 중 예약")
         print(f"예약 대기 사용: {self.want_reserve}")
 
-        # 조회하기 버튼 클릭
         self.driver.find_element(By.XPATH, "//input[@value='조회하기']").click()
         self.driver.implicitly_wait(5)
         time.sleep(1)
 
-    def refresh_search_result(self):
+    def book_ticket(self, standard_seat, i):
+        # standard_seat는 일반석 검색 결과 텍스트
+        
+        if "예약하기" in standard_seat:
+            print("예약 가능 클릭")
+
+            # Error handling in case that click does not work
+            try:
+                self.driver.find_element(By.CSS_SELECTOR,
+                                         f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7) > a").click()
+            except ElementClickInterceptedException as err:
+                print(err)
+                self.driver.find_element(By.CSS_SELECTOR,
+                                         f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7) > a").send_keys(
+                    Keys.ENTER)
+            finally:
+                self.driver.implicitly_wait(3)
+
+            # 예약이 성공하면
+            if self.driver.find_elements(By.ID, 'isFalseGotoMain'):
+                self.is_booked = True
+                print("예약 성공")
+                return self.driver
+            else:
+                print("잔여석 없음. 다시 검색")
+                self.driver.back()  # 뒤로가기
+                self.driver.implicitly_wait(5)
+
+    def refresh_result(self):
+        submit = self.driver.find_element(By.XPATH, "//input[@value='조회하기']")
+        self.driver.execute_script("arguments[0].click();", submit)
+        self.cnt_refresh += 1
+        print(f"새로고침 {self.cnt_refresh}회")
+        self.driver.implicitly_wait(10)
+        time.sleep(0.5)
+
+    def reserve_ticket(self, reservation, i):
+        if "신청하기" in reservation:
+            print("예약 대기 완료")
+            self.driver.find_element(By.CSS_SELECTOR,
+                                     f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(8) > a").click()
+            self.is_booked = True
+            return self.driver
+
+    def check_result(self):
         while True:
             for i in range(1, self.num_trains_to_check+1):
                 try:
@@ -131,59 +169,28 @@ class SRT:
                     standard_seat = "매진"
                     reservation = "매진"
 
-                if "예약하기" in standard_seat:
-                    print("예약 가능 클릭")
-
-                    # Error handling in case that click does not work
-                    try:
-                        self.driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7) > a").click()
-                    except ElementClickInterceptedException as err:
-                        print(err)
-                        self.driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(7) > a").send_keys(Keys.ENTER)
-                    finally:
-                        self.driver.implicitly_wait(3)
-
-                    # 예약이 성공하면
-                    if self.driver.find_elements(By.ID, 'isFalseGotoMain'):
-                        is_booked = True
-                        print("예약 성공")
-                        return self.driver
-                    else:
-                        print("잔여석 없음. 다시 검색")
-                        self.driver.back()  # 뒤로가기
-                        self.driver.implicitly_wait(5)
-
+                self.book_ticket(standard_seat, i)
                 if self.want_reserve:
-                    if "신청하기" in reservation:
-                        print("예약 대기 완료")
-                        self.driver.find_element(By.CSS_SELECTOR, f"#result-form > fieldset > div.tbl_wrap.th_thead > table > tbody > tr:nth-child({i}) > td:nth-child(8) > a").click()
-                        is_booked = True
-                        return self.driver
+                    self.reserve_ticket(reservation, i)
 
-            if not self.is_booked:
-                time.sleep(randint(2, 4))  # 2~4초 랜덤으로 기다리기
-
-                # 다시 조회하기
-                submit = self.driver.find_element(By.XPATH, "//input[@value='조회하기']")
-                self.driver.execute_script("arguments[0].click();", submit)
-                self.cnt_refresh += 1
-                print(f"새로고침 {self.cnt_refresh}회")
-                self.driver.implicitly_wait(10)
-                time.sleep(0.5)
-            else:
+            if self.is_booked:
                 return self.driver
+            else:
+                time.sleep(randint(2, 4))
+                self.refresh_result()
 
     def run(self, login_id, login_psw):
         self.run_driver()
         self.set_log_info(login_id, login_psw)
         self.login()
         self.go_search()
-        self.refresh_search_result()
+        self.check_result()
 
-
+#
 # if __name__ == "__main__":
 #     srt_id = os.environ.get('srt_id')
 #     srt_psw = os.environ.get('srt_psw')
 #
-#     srt = SRT("동탄", "동대구", "20220119", "08")
+#     srt = SRT("동탄", "동대구", "20220917", "08")
 #     srt.run(srt_id, srt_psw)
+
